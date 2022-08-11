7dd68151c3a8fb66469624fc58b6561944e81fb0
TheSpeakeR23 <zaharov20101998@gmail.com>
2022-08-11 16:37:08 +0300

    Add Example 005 Condition

--- C#/Лекции/Example_005_Condition/Program.cs
+++ C#/Лекции/Example_005_Condition/Program.cs
@@ -1,12 +1,11 @@
-﻿Console.Clear();
-Console.Write("Введите имя пользователя: ");
-string username = Console.ReadLine();
+﻿//Console.Clear();
+//Console.Write("Введите имя пользователя: ");
+//string username = Console.ReadLine();
 
-if (username == "Николай")
-{
-    Console.WriteLine("Ура, это же НИКОЛАЙ!");
-}
-else
-{
-    Console.WriteLine($"Привет, {username}!");
-}
+//if (username == "Николай")
+
+    //Console.WriteLine("Ура, это же НИКОЛАЙ!");
+
+//else
+
+    //Console.WriteLine($"Привет, {username}!");
