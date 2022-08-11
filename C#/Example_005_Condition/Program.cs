0ba47e6c6f731ab4167ebae77cfac6dca918e4ab
TheSpeakeR23 <zaharov20101998@gmail.com>
2022-08-11 15:32:19 +0300

    Add Example 005 Condition

---
+++ C#/Example_005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "николай")
+{
+    Console.WriteLine("Ура, это же НИКОЛАЙ!");
+}
+else
+{
+    Console.WriteLine($"Привет, {username}!");
+}
