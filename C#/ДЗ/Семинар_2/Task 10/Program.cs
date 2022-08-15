6f51469b7cc5045bd76e3db5684249cb7b3a5e08
TheSpeakeR23 <zaharov20101998@gmail.com>
2022-08-15 18:57:33 +0300

    Add task 10 to homework

--- C#/ДЗ/Семинар_2/Task 10/Program.cs
+++ C#/ДЗ/Семинар_2/Task 10/Program.cs
@@ -1,5 +1,18 @@
 ﻿Console.Clear();
 Console.Write("Введите трёхзначное число: ");
 int Number = Convert.ToInt32(Console.ReadLine());
-string stringNumber = Convert.ToString(Number);
-Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+
+if (Number > 99 & Number < 1000)
+{    
+    string stringNumber = Convert.ToString(Number);
+    Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+}
+else if (Number < -99 & Number > -1000)
+{
+    string stringNumber = Convert.ToString(Number);
+    Console.WriteLine("вторая цифра этого числа -> "+stringNumber[2]);
+}
+else
+{
+    Console.WriteLine("Ошибка ввода данных!");
+}
