c8d7194dd326dc29ac660b75426ab0ed331d388f
TheSpeakeR23 <zaharov20101998@gmail.com>
2022-08-15 19:05:49 +0300

    Add task 13 to homework

--- C#/ДЗ/Семинар_2/Task 13/Program.cs
+++ C#/ДЗ/Семинар_2/Task 13/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите любое число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number > 99)
+{    
+    string stringNumber = Convert.ToString(Number);
+    Console.WriteLine("Третья цифра этого числа -> "+stringNumber[2]);
+}
+else if (Number < -99)
+{
+    string stringNumber = Convert.ToString(Number);
+    Console.WriteLine("Третья цифра этого числа -> "+stringNumber[3]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
