6b314ecf7067c0dd298462aa446c17b302ce21fd
TheSpeakeR23 <zaharov20101998@gmail.com>
2022-08-15 19:29:07 +0300

    Add task 15 to homework

---
+++ C#/ДЗ/Семинар_2/Task 15/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+Console.Write("Введите какой сегодня день недели от 1 до 7: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number >= 1 & Number <= 7)
+{
+    if (Number == 7)
+    {
+        Console.WriteLine("Сегодня воскресенье - выходной день.");
+    }
+    else if (Number == 6)
+    {
+        Console.WriteLine("Сегодня суббота - выходной день.");
+    }
+    else if (Number == 5)
+    {
+        Console.WriteLine("Сегодня пятница - рабочий день.");
+    }
+    else if (Number == 4)
+    {
+        Console.WriteLine("Сегодня четверг - рабочий день.");
+    }
+    else if (Number == 3)
+    {
+        Console.WriteLine("Сегодня среда - рабочий день.");
+    }
+    else if (Number == 2)
+    {
+        Console.WriteLine("Сегодня вторник - рабочий день.");
+    }
+    else if (Number == 1)
+    {
+        Console.WriteLine("Сегодня понедельник - начало рабочей недели.");
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка ввода дынных!");
+}
