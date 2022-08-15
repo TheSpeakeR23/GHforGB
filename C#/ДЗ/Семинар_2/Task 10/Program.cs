7dd9065c4b7094f73ee33dd5cd050d44dd355472
TheSpeakeR23 <zaharov20101998@gmail.com>
2022-08-15 19:08:09 +0300

    Add task 10 to homework

--- C#/ДЗ/Семинар_2/Task 10/Program.cs
+++ C#/ДЗ/Семинар_2/Task 10/Program.cs
@@ -5,12 +5,12 @@ int Number = Convert.ToInt32(Console.ReadLine());
 if (Number > 99 & Number < 1000)
 {    
     string stringNumber = Convert.ToString(Number);
-    Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+    Console.WriteLine("Вторая цифра этого числа -> "+stringNumber[1]);
 }
 else if (Number < -99 & Number > -1000)
 {
     string stringNumber = Convert.ToString(Number);
-    Console.WriteLine("вторая цифра этого числа -> "+stringNumber[2]);
+    Console.WriteLine("Вторая цифра этого числа -> "+stringNumber[2]);
 }
 else
 {
