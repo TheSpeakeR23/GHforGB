18ad2022419877ce20d2ba01340fa859fa140800
TheSpeakeR23 <zaharov20101998@gmail.com>
2022-08-30 17:59:29 +0300

    Add task 27

---
+++ C#/ДЗ/Семинар_4/Task27/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int Number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+int Summa(int Number) {
+    while (Number > 0) {
+    int num = Number % 10;
+    Number = Number / 10;
+    sum = sum + num;
+    }
+return sum;
+}
+
+sum = Summa(Number);
+Console.WriteLine($"Cумма всех цифр числа {Number} равна: {sum}");
