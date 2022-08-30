0d2eff72ad9890129ceccecae3580090e6ddfe98
TheSpeakeR23 <zaharov20101998@gmail.com>
2022-08-30 18:38:03 +0300

    Add task 27

---
+++ C#/ДЗ/Семинар_4/Task29/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите количество элементов N в массиве: ");
+int N = int.Parse(Console.ReadLine());
+int[] myArray = new int[N];
+for (int i = 0; i < myArray.Length; i++) {
+    Console.Write("Введите {0}-й элемент: ", i + 1);
+    myArray[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Вывод массива:");
+for (int i = 0; i < myArray.Length; i++) {
+    Console.Write($"{myArray[i]}, ");
+}
+
+
