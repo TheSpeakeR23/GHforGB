e73c62af9a922a7e7e21fc4ea0d10478010ecc77
TheSpeakeR23 <zaharov20101998@gmail.com>
2022-08-30 17:19:00 +0300

    Add 23 task

---
+++ C#/ДЗ/Семинар_3/Task23/Program.cs
@@ -0,0 +1,14 @@
+﻿int main() {
+    Console.Write("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int count = 1;
+    double cube;
+    while (count <= number) {
+        cube = Math.Pow(count, 3);
+        Console.WriteLine(cube);
+        count++;
+    }
+return 0;
+}
+
+main();
