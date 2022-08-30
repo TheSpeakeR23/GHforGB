4f433e3a302285a5a46c3a3a2e3a229e1eb40f1b
TheSpeakeR23 <zaharov20101998@gmail.com>
2022-08-30 15:50:18 +0300

    Add 19 task

---
+++ C#/ДЗ/Семинар_3/Task19/Program.cs
@@ -0,0 +1,19 @@
+﻿int main() {
+    int Number;
+    string StrNumber;
+    Console.Write("Введите пятизначное целое число: ");
+    Number = Convert.ToInt32(Console.ReadLine());
+    if (Number > 9999 && Number < 100000) {
+        StrNumber = Convert.ToString(Number);
+        if (StrNumber[0] == StrNumber[4] && StrNumber[1] == StrNumber[3]) {
+            Console.WriteLine($"Ваше число: {Number} - является палиндромом");
+        } else {
+            Console.WriteLine($"Ваше число: {Number} - НЕ является палиндромом");
+        }
+    } else {
+        Console.WriteLine("Некорректный ввод данных. Введите пятизначное целое число.");
+    }
+return 0;
+}
+
+main();
