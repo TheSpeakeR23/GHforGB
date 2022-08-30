ece625d7840cd4a52043bbf857f870fe69bdb256
TheSpeakeR23 <zaharov20101998@gmail.com>
2022-08-30 17:48:12 +0300

    Add task 25

---
+++ C#/ДЗ/Семинар_4/Task25/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int step = a;
+
+int cycle(int a, int b) {
+    for (int i = 1; i < b; i++)
+    {
+    step = step * a;
+    }
+return step;
+}
+
+step = cycle(a, b);
+Console.WriteLine($"{a} в степени {b} равно: {step}");
