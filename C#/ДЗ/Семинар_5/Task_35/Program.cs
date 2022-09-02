7534cae2ae018a3b832d7e8e74d64576b923853b
TheSpeakeR23 <zaharov20101998@gmail.com>
2022-09-02 14:19:15 +0300

    Rename directory

---
+++ C#/ДЗ/Семинар_5/Task_35/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+
+int[] numbers = new int[123];
+
+void input_numbers (int[] Array, int min, int max){
+  for (int i = 0; i < Array.Length; i++ ){
+    Array[i] = new Random().Next(min, max);
+  }
+}
+
+void WriteArray(int[] Array){
+    for (int i = 0; i<Array.Length; i++ ){
+    Console.Write(Array[i] + " ");
+  }
+  Console.WriteLine();
+}
+
+int Quantity(int[] Array){
+    int quantity = 0;
+    for (int i = 0; i<Array.Length; i++ ){
+    if (Array[i] >= 10 && Array[i] <= 99)
+    {
+      quantity++;
+    }
+  }
+  return quantity;
+}
+
+input_numbers(numbers, 10, 600);
+WriteArray(numbers);
+Console.WriteLine();
+
+int quantity = Quantity(numbers);
+Console.WriteLine($"Количество элементов массива, значения которых лежат в отрезке [10,99]: {quantity}");
+
