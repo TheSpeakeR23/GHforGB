6874489b6315cbb4a25d3db13490245824e7a492
TheSpeakeR23 <zaharov20101998@gmail.com>
2022-09-02 13:47:58 +0300

    Add 29 task

--- C#/ДЗ/Семинар_4/Task29/Program.cs
+++ C#/ДЗ/Семинар_4/Task29/Program.cs
@@ -1,13 +1,23 @@
-﻿Console.Write("Введите количество элементов N в массиве: ");
-int N = int.Parse(Console.ReadLine());
-int[] myArray = new int[N];
-for (int i = 0; i < myArray.Length; i++) {
-    Console.Write("Введите {0}-й элемент: ", i + 1);
-    myArray[i] = int.Parse(Console.ReadLine());
-}
-Console.WriteLine("Вывод массива:");
-for (int i = 0; i < myArray.Length; i++) {
-    Console.Write($"{myArray[i]}, ");
+﻿int main() {
+    Console.Write("Введите количество элементов N в массиве: ");
+    int N = int.Parse(Console.ReadLine());
+    if (N > 0) {
+        int[] Array = new int[N];
+        for (int i = 0; i < Array.Length; i++) {
+            Console.Write("Введите {0}-й элемент: ", i + 1);
+            Array[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.Write("Вывод массива: ");
+        for (int i = 0; i < Array.Length; i++) {
+            Console.Write($"{Array[i]} ");
+        }
+
+    } else {
+        Console.WriteLine("Некорректный ввод данных");
+    }
+    return 0;
 }
 
+main();
 
