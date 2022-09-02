4fe8de01d52838cb2934d4ebf99ccef667ff99e3
TheSpeakeR23 <zaharov20101998@gmail.com>
2022-09-02 14:46:28 +0300

    Add task 36

---
+++ C#/ДЗ/Семинар_5/Task_36/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+
+Console.Write($"Введи количество элементов массива N: ");
+int N = Convert.ToInt32(Console.ReadLine()); 
+
+int RandomNumbers(int N, int min, int max)
+  {
+  int[] randomArray = new int[N];
+  int SumNumbers = 0;
+  Console.Write("Получившийся массив: ");
+
+    for (int i = 0; i <randomArray.Length; i++ ){
+      randomArray[i] = new Random().Next(min, max);
+
+      Console.Write(randomArray[i] + " ");
+
+      if (i % 2 != 1)
+      {
+        SumNumbers = SumNumbers + randomArray[i];
+      }
+    }
+  return SumNumbers;
+  }
+
+int randomArray =  RandomNumbers(N, 1, 10);
+
+Console.WriteLine($"\nСумма элементов, стоящих на нечётных позициях: {randomArray}");
