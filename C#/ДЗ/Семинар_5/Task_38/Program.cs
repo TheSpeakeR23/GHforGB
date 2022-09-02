7e25246b991b3df4f653c0509951fbeee379b736
TheSpeakeR23 <zaharov20101998@gmail.com>
2022-09-02 14:50:59 +0300

    Add 38 task

---
+++ C#/ДЗ/Семинар_5/Task_38/Program.cs
@@ -0,0 +1,25 @@
+﻿double[] Array = new double[10];
+  for (int i = 0; i < Array.Length; i++ )
+  {
+    Array[i] = new Random().Next(1, 10);
+    Console.Write(Array[i] + " ");
+  }
+
+double maxNumber = Array[0];
+double minNumber = Array[0];
+
+  for (int i = 1; i < Array.Length; i++)
+  {
+    if (maxNumber < Array[i])
+    {
+      maxNumber = Array[i];
+    }
+        if (minNumber > Array[i])
+    {
+      minNumber = Array[i];
+    }
+  }
+
+  double decision = maxNumber - minNumber;
+
+  Console.WriteLine($"\nРазница между между максимальным ({maxNumber}) и минимальным({minNumber}) элементами массива: {decision}");
