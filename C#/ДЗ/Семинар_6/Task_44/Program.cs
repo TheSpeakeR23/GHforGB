4ebc31718c12da39ea3ddad91cfb67ad5f235220
TheSpeakeR23 <zaharov20101998@gmail.com>
2022-09-10 13:00:20 +0300

    Task44

---
+++ C#/ДЗ/Семинар_6/Task_44/Program.cs
@@ -0,0 +1,10 @@
+﻿int n, first = 0, second = 1, third = 0;
+Console.Write("Сколько первых элементов ряда Фибоначчи вывести на экран : ");
+n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Первые {0} элементов ряда Фибоначчи {1} {2} ", n, first, second);
+    for (int i = 3; i <= n; i++) {
+        third = first + second;
+            Console.Write("{0} ", third);
+            first = second;
+            second = third;
+        }
