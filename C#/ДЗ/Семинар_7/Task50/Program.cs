07bcff86686b4d8c9e7bb429c79561c84e9a544e
TheSpeakeR23 <zaharov20101998@gmail.com>
2022-09-12 15:45:57 +0300

    Complete 50 task

--- C#/ДЗ/Семинар_7/Task50/Program.cs
+++ C#/ДЗ/Семинар_7/Task50/Program.cs
@@ -7,16 +7,14 @@ FillArrayRandomNumbers(numbers);
 PrintArray(numbers);
 
 int ElementLine = ReadInt("Введите номер строки элемента: ");
-
 int ElementColumn = ReadInt("Введите номер столбца элемента: ");
-
 if (ElementLine - 1 < numbers.GetLength(0) && ElementColumn - 1 < numbers.GetLength(1)) Console.WriteLine(numbers[ElementLine - 1, ElementColumn - 1]);
 else Console.WriteLine($"{ElementLine}{ElementColumn} -> такого числа в массиве нет");
 
 void FillArrayRandomNumbers(double[,] array) {
     for (int i = 0; i < array.GetLength(0); i++) {
         for (int j = 0; j < array.GetLength(1); j++) {
-            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100));
         }
     }
 }
@@ -31,8 +29,7 @@ void PrintArray(double[,] array) {
     Console.WriteLine();
 }
 
-int ReadInt(string message)
-{
+int ReadInt(string message) {
     Console.Write(message);
     return Convert.ToInt32(Console.ReadLine());
 }
