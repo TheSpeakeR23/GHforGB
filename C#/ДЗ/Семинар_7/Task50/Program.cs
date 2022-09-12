1ff76eb87a95ba6e1e0ae3c1ded6046d95ce0821
TheSpeakeR23 <zaharov20101998@gmail.com>
2022-09-12 13:42:24 +0300

    Complete Task 50

---
+++ C#/ДЗ/Семинар_7/Task50/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Write("Введите количество строк: ");
+int ArrLines = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int ArrColumns = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[ArrLines, ArrColumns];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+int ElementLine = ReadInt("Введите номер строки: ");
+
+int ElementColumn = ReadInt("Введите номер столбца: ");
+
+if (ElementLine - 1 < numbers.GetLength(0) && ElementColumn - 1 < numbers.GetLength(1)) Console.WriteLine(numbers[ElementLine - 1, ElementColumn - 1]);
+else Console.WriteLine($"{ElementLine}{ElementColumn} -> такого числа в массиве нет");
+
+void FillArrayRandomNumbers(double[,] array) {
+    for (int i = 0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] array) {
+    for (int i = 0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) {
+            Console.Write(array[i, j] + " \t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
