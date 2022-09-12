a1c14df83df561970c76718ed6a109cf887df0c0
TheSpeakeR23 <zaharov20101998@gmail.com>
2022-09-12 13:14:25 +0300

    Complete Task 47

---
+++ C#/ДЗ/Семинар_7/Task47/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите количество строк");
+int ArrLines = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int ArrColumns = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[ArrLines, ArrColumns];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(double[,] array) {
+    for (int i = 0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] array) {
+    for (int i = 0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) {
+            Console.Write(array[i, j] + " \t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
