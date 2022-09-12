bf110ff0a34e5fb7e8a354acbfdc21474c083dfa
TheSpeakeR23 <zaharov20101998@gmail.com>
2022-09-12 15:43:14 +0300

    Complete 52 task

--- C#/ДЗ/Семинар_7/Task52/Program.cs
+++ C#/ДЗ/Семинар_7/Task52/Program.cs
@@ -1,16 +1,17 @@
-﻿Console.Write("Введите количество строк: ");
+﻿Console.Write("Введите количество строк массива: ");
 int ArrLines = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите количество столбцов: ");
+Console.Write("Введите количество столбцов массива: ");
 int ArrColumns = Convert.ToInt32(Console.ReadLine());
 double[,] numbers = new double[ArrLines, ArrColumns];
 FillArrayRandomNumbers(numbers);
 PrintArray(numbers);
+Console.WriteLine("Среднее арифметическое элементов в каждом столбце:");
 ArithmeticMean(numbers);
 
 void FillArrayRandomNumbers(double[,] array) {
     for (int i = 0; i < array.GetLength(0); i++) {
         for (int j = 0; j < array.GetLength(1); j++) {
-            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100));
         }
     }
 }
@@ -29,8 +30,9 @@ void ArithmeticMean(double[,] array) {
     for (int j = 0; j < array.GetLength(1); j++) {
         double sum = 0;
         for (int i = 0; i < array.GetLength(0); i++) {
-            sum += array[i, j];
+            sum += array[i, j]; 
         }
-    Console.Write($"{ sum / array.GetLength(0)} ");
+        double result = Math.Round(sum / array.GetLength(0), 2);
+    Console.Write($"{result}\t");
     }
 }
