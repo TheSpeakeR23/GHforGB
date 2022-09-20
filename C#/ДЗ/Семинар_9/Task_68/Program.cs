d2e4b8b2be33f492d75d4480743f5baced1f821f
TheSpeakeR23 <zaharov20101998@gmail.com>
2022-09-20 14:13:11 +0300

    Complete task 68

---
+++ C#/ДЗ/Семинар_9/Task_68/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+
+Console.Write("Введите значение числа M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение числа N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (m >= 0 && n >= 0) {
+    Console.Write($"Функция Аккермана для чисел A({m},{n}) = ");
+    AkkermanFunction(m,n);
+} else {
+    Console.WriteLine("Некорректный ввод данных...");    
+}
+
+void AkkermanFunction(int m, int n) {
+    Console.Write(Akkerman(m, n)); 
+}
+
+// функция Аккермана
+int Akkerman(int m, int n) {
+    if (m == 0) {
+        return n + 1;
+    } else if (n == 0 && m > 0) {
+        return Akkerman(m - 1, 1);
+    } else {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
