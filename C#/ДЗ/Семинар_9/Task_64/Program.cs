0745e029300f004bb23d24e502726e23938c3b89
TheSpeakeR23 <zaharov20101998@gmail.com>
2022-09-20 13:52:46 +0300

    Complete task 64

---
+++ C#/ДЗ/Семинар_9/Task_64/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.Write("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+if (number >= 1) {
+    Console.WriteLine($"Последовательность натуральных чисел от {number} до 1:");
+    NaturalToLow(number, count);
+}
+else {
+    Console.WriteLine("Некорректный ввод данных...");
+}
+
+void NaturalToLow(int n, int count) {
+    if (count > n) {
+        return;
+    } else {
+        NaturalToLow(n, count + 1);
+        Console.Write(count + " ");
+    }
+}
