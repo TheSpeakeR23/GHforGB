32f8c053dbdb049c6a975fb81eac94a9bfbb4a6b
TheSpeakeR23 <zaharov20101998@gmail.com>
2022-09-22 20:04:57 +0300

    lection 1 python

---
+++ Python/Лекции/Лекция_1/hello.py
@@ -0,0 +1,16 @@
+# a = 123
+# b = 1.23
+# print(type(a))
+# print(b)
+# s = 'hello "world"!'
+# print(a, b, s)
+
+# list = ['1', '2', '3' ]
+# print(list)
+
+print('Введите значение а:')
+a = float(input())
+print('Введите значение b')
+b = float(input())
+print(f'{a} {b}')
+print('{} {}'.format(a, b))
