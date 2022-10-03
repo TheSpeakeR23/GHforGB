3eb3376eb72946ad3bbef6a5716d6ad8b871b873
TheSpeakeR23 <zaharov20101998@gmail.com>
2022-10-03 16:33:33 +0300

    Complete Task 1

--- Python/Seminars/HW_3/Task1.py
+++ Python/Seminars/HW_3/Task1.py
@@ -1,17 +1,42 @@
 import random
 
-def elements_sum():
-    n = int(input('Введите количество элементов в списке: '))
-    if n > 0:
-        rand_list = random.sample(range(1, 100), n)
-        print(f'Список из произвольных чисел: {rand_list}')
-        sum = 0
-        for i in range(len(rand_list)):
-            if i % 2 == 0:
-                sum += rand_list[i]
-        print(f'Сумма элементов, стоящих на нечетных позициях, равна: {sum}')
-    else:
-        print('Неправильный ввод количества элементов в списке')
+# def elements_sum():
+#     n = int(input('Введите количество элементов в списке: '))
+#     if n > 0:
+#         rand_list = random.sample(range(1, 100), n)
+#         print(f'Список из произвольных чисел: {rand_list}')
+#         sum = 0
+#         for i in range(n):
+#             if i % 2 == 0:
+#                 sum += rand_list[i]
+#         print(f'Сумма элементов, стоящих на нечетных позициях, равна: {sum}')
+#     else:
+#         print('Неправильный ввод количества элементов в списке')
 
-elements_sum() 
+# elements_sum() 
 
+def random_list(n):
+    rand_list = random.sample(range(1, 100), n)
+    return rand_list 
+
+
+def sum_of_odd_elements(rand_list):
+    sum_odd = 0
+    for i in range(n):
+        if i % 2 == 0:
+            sum_odd += rand_list[i]
+    return sum_odd
+
+def sum_of_even_elements(rand_list):
+    sum_odd = 0
+    for i in range(n):
+        if i % 2 != 0:
+            sum_odd += rand_list[i]
+    return sum_odd  
+
+n = int(input('Введите количество элементов в списке: '))
+if n > 0:
+    rand_list = random_list(n)
+    print(f'Список из произвольных чисел: {rand_list}')
+    print(f'Сумма элементов, стоящих на нечетных позициях, равна: {sum_of_odd_elements(rand_list)}')
+    print(f'Сумма элементов, стоящих на четных позициях, равна: {sum_of_even_elements(rand_list)}')
