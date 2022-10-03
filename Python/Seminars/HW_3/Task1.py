7d896f28b7c9169ae379eb8a34f5316f537bb214
TheSpeakeR23 <zaharov20101998@gmail.com>
2022-10-03 15:53:18 +0300

    Complete Task 1

--- Python/Seminars/HW_3/Task1.py
+++ Python/Seminars/HW_3/Task1.py
@@ -1,14 +1,17 @@
 import random
 
 def elements_sum():
-    n = int(input('Введите количество элементов в массиве: '))
-    rand_list = random.sample(range(1, 100), n)
-    print(f'Список из произвольных чисел: {rand_list}')
-    sum = 0
-    for i in range(len(rand_list)):
-        if i % 2 == 0:
-            sum += rand_list[i]
-    print(sum)
+    n = int(input('Введите количество элементов в списке: '))
+    if n > 0:
+        rand_list = random.sample(range(1, 100), n)
+        print(f'Список из произвольных чисел: {rand_list}')
+        sum = 0
+        for i in range(len(rand_list)):
+            if i % 2 == 0:
+                sum += rand_list[i]
+        print(f'Сумма элементов, стоящих на нечетных позициях, равна: {sum}')
+    else:
+        print('Неправильный ввод количества элементов в списке')
 
 elements_sum() 
 
